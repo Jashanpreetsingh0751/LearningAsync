0581980ff4c9d9f5992b05ffd66aa3d5a391eab0
visualstudio <visualstudio@122-PC22>
2019-03-27 10:28:00 -0400

    10:30

--- LearningASync/Program.cs
+++ LearningASync/Program.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace LearningASync
+namespace LearningAsync
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Downloading file");
+            Download();
+            Console.ReadLine();
+        }
+
+        static void Download()
+        {
+            Task.Run(() => {
+                Thread.Sleep(3000);
+                Console.WriteLine("Download Complete");
+            });
+
         }
     }
 }
